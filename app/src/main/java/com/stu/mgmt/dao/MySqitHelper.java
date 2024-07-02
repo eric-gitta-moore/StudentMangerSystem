63474eebe213b237bd9c86a5e3b57edb3884d775
Eric Moore <EricGittaMoore@duck.com>
2024-07-02 21:55:30 +0800

    refactor: 整理代码

--- app/src/main/java/com/stu/mgmt/dao/MySqitHelper.java
+++ app/src/main/java/com/stu/mgmt/dao/MySqitHelper.java
@@ -7,21 +7,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MySqitHelper extends SQLiteOpenHelper {
     //
-	static String DB_NAME="studentsystem.db";
-	static int version=1;
-	//构造方法 
-	public MySqitHelper(Context context){
-		super(context,DB_NAME,null,version);
-	}
-	@Override
-	public void onCreate(SQLiteDatabase arg0) {
-		// TODO Auto-generated method stub
+    static String DB_NAME = "studentsystem.db";
+    static int version = 1;
 
-	}
-	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
+    //构造方法
+    public MySqitHelper(Context context) {
+        super(context, DB_NAME, null, version);
+    }
 
-	}
+    @Override
+    public void onCreate(SQLiteDatabase arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
